dd16311007c22148a83b729de6a335ece9d195b9
sen.lyu <sen.lyu@uconn.edu>
2016-11-26 10:19:47 -0500

    Update sqlmypart.sql

--- sqlmypart.sql
+++ sqlmypart.sql
@@ -1,18 +1,18 @@
--- 1
+-- 1 Conversion Functions and Conditional Expressions
 select lastname, to_char(dateofbirth,'fmdd Month YYYY') as birthday, NVL(Homeaddress,'Dont Know')
 from customer;
--- 12
+-- 12 Conversion Functions and Conditional Expressions
 select ride_id, distance,decode(sign(distance-10),-1, 1,1,0,0,1)
 revised_distance
 from riderecord1;
---2
+--2 join
 SELECT last_name,
   first_name,
   car_name_plate,
   car_type
 FROM driver
 JOIN car USING(driver_id);
---3
+--3 subquery
 SELECT last_name,
   first_name,
   car_name_plate,
@@ -22,8 +22,5 @@ JOIN car USING(driver_id)
 join Riderecord1 using(driver_id)
 where total_fare> (
     select avg(total_fare) from riderecord1);
---4
-update customer
-set homeaddress='100 Constitution Plaza'
-where customer_id=1;
+
 
